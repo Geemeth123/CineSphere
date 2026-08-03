121116025c8488ff67276c0cc5f7534e59b6e1e7
Perfee123 <kdulkethd13@gmail.com>
2026-08-03 20:14:26 +0530

    New fix

--- src/utils/DBUtils.java
+++ src/utils/DBUtils.java
@@ -40,6 +40,11 @@ public class DBUtils {
     }
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("MySQL JDBC Driver not found: " + e.getMessage());
+        }
         String url = env.getOrDefault("DB_URL", "jdbc:mysql://localhost:3306/cinesphere");
         String user = env.getOrDefault("DB_USER", "root");
         String password = env.getOrDefault("DB_PASSWORD", "");
